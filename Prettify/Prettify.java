01749dc536b5cbf903a4ad85f7fa2a8711ab81da
VenkataRamanaRao5 <162092344+VenkataRamanaRao5@users.noreply.github.com>
2024-09-13 15:52:53 +0530

    Create Prettify.java

---
+++ Prettify/Prettify.java
@@ -0,0 +1,75 @@
+import java.io.File                                                                                          ;
+import java.io.FileNotFoundException                                                                         ;
+import java.io.PrintWriter                                                                                   ;
+import java.util.ArrayList                                                                                   ;
+import java.util.Arrays                                                                                      ;
+import java.util.Scanner                                                                                     ;
+public class Prettify                                                                                        {
+    private static int lineWidth = 80, tabSize = 4                                                           ;
+    private static String indent(StringBuilder buffer, int indentation)                                      {
+        int pad = lineWidth - 1 - (tabSize * indentation) - buffer.length()                                  ;
+        return " ".repeat(tabSize).repeat(indentation) + buffer +                                            
+        " ".repeat(pad > 0 ? pad : 0)                                                                        ;
+                                                                                                             }
+    private static void prettify(String fname) throws FileNotFoundException                                  {
+        Scanner inputFile = new Scanner(new File(fname))                                                     ;
+        ArrayList<String> lines = new ArrayList<>()                                                          ;
+        StringBuilder buffer = new StringBuilder("")                                                         ;
+        int indentation = 0                                                                                  ;
+        while (inputFile.hasNextLine())                                                                      {
+            String line = inputFile.nextLine().trim().replaceAll(" {4,}([{};])$", "$1")                      ;
+            Boolean isQuoted = false, isEscaped = false                                                      ;
+            for (char ch : line.toCharArray())                                                               {
+                if (isEscaped)isEscaped = false                                                              ;
+                else if (isQuoted)                                                                           {
+                    if (ch == '\"' || ch == '\'')isQuoted = false                                            ;
+                    else if (ch == '\\')isEscaped = true                                                     ;
+                                                                                                             }
+                else if (ch == '\"' || ch == '\'')isQuoted = true                                            ;
+                else if (ch == '{' || ch == '}' || ch == ';')                                                {
+                    lines.add(indent(buffer, indentation)+ch)                                                ;
+                    buffer = new StringBuilder("")                                                           ;
+                    if (ch == '{')indentation++                                                              ;
+                    else if (ch == '}')indentation--                                                         ;
+                    continue                                                                                 ;
+                                                                                                             }
+                buffer.append(ch)                                                                            ;
+                                                                                                             }
+            if(!buffer.toString().isBlank())                                                                 {
+                lines.add(indent(buffer, indentation))                                                       ;
+                buffer = new StringBuilder("")                                                               ;
+                                                                                                             }
+                                                                                                             }
+        inputFile.close()                                                                                    ;
+        PrintWriter outputFile = new PrintWriter(fname)                                                      ;
+        for (String s : lines)                                                                               {
+            outputFile.println(s)                                                                            ;
+                                                                                                             }
+        outputFile.close()                                                                                   ;
+                                                                                                             }
+    public static void main(String[] args) throws Exception                                                  {
+        System.out.println(Arrays.toString(args))                                                            ;
+        if(args.length == 0) throw new Exception("File name needed\n")                                       ;
+        else if(args[0].equals("-i") || args[0].equals("--inputFile"))                                       {
+            Scanner sc = new Scanner(new File(args[1]))                                                      ;
+            lineWidth = Integer.parseInt(args[2])                                                            ;
+            while(sc.hasNextLine())                                                                          {
+                prettify(sc.nextLine().trim())                                                               ;
+                                                                                                             }
+            sc.close()                                                                                       ;
+                                                                                                             }
+        else if(args[0].equals("--help") || args[0].equals("-h"))                                            {
+            System.out.println("Prettify - code formatter for braces and semicolon based languages")         ;
+            System.out.println("Usage: Prettify [options] [file] [linewidth]")                               ;
+            System.out.println("-i --inputFile: Readable file containing paths of the files to be formatted");
+            System.out.println("-h --help: Displays this help")                                              ;
+            System.out.println("file: The file of filenames if -i option is selected")                       ;
+            System.out.println("\t else the input file to be formatted")                                     ;
+            System.out.println("linewidth: the max width of the line (no wrap)")                             ;
+                                                                                                             }
+        else if(args.length == 2)                                                                            {
+            lineWidth = Integer.parseInt(args[1])                                                            ;
+            prettify(args[0])                                                                                ;
+                                                                                                             }
+                                                                                                             }
+                                                                                                             }
